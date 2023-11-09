5634952ee50a97b17f49a1088f2169362e6b3c02
peace_akame <blackstile2014@gmail.com>
2023-11-10 00:04:24 +0200

    update

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ if __name__ == '__main__':
         if sys.argv[i] == '--ip':
             ip_address = sys.argv[i + 1]
         elif sys.argv[i] == '--port':
-            port = int(sys.argv[i + 1])
+            port = int(sys.argv[i + 2])
     app.run(host=ip_address, port=port)
 
 
