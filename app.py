9b007a3e3354d3d6b24648e0ce383f6d55c109df
peace_akame <blackstile2014@gmail.com>
2023-11-09 21:32:20 +0200

    simple-http-server

---
+++ app.py
@@ -0,0 +1,58 @@
+import os
+import sys
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from datetime import datetime
+
+app = Flask(__name__)
+
+def get_directory_contents(path):
+    items = os.listdir(path)
+    files = []
+    folders = []
+
+    for item in items:
+        item_path = os.path.join(path, item)
+        last_modified = datetime.fromtimestamp(os.path.getmtime(item_path)).strftime('%Y-%m-%d %H:%M:%S')
+        if os.path.isfile(item_path):
+            size = os.path.getsize(item_path)
+            files.append((item, last_modified, size))
+        else:
+            folders.append((item, last_modified))
+
+    return folders, files
+
+@app.route('/')
+def list_directory():
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    folders, files = get_directory_contents(current_directory)
+    return render_template('index.html', folders=folders, files=files, path="")
+
+@app.route('/directory/<path:subpath>')
+def list_subdirectory(subpath):
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    directory_path = os.path.join(current_directory, subpath)
+    folders, files = get_directory_contents(directory_path)
+    return render_template('index.html', folders=folders, files=files, path=subpath)
+
+@app.route('/download/<path:filename>')
+def download_file(filename):
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(current_directory, filename)
+    return send_from_directory(current_directory, filename, as_attachment=True)
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' in request.files:
+        uploaded_file = request.files['file']
+        if uploaded_file.filename != '':
+            file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), uploaded_file.filename)
+            uploaded_file.save(file_path)
+    return redirect(url_for('list_directory'))
+
+if __name__ == '__main__':
+    ip_address = '127.0.0.1' # Replace localhost ip with the desired ip to run on it by default
+    if len(sys.argv) > 1 and sys.argv[1] == '--ip':
+        ip_address = sys.argv[2]
+    app.run(host=ip_address, port=5000)
+
+
